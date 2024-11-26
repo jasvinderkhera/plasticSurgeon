07cdc11a015f3cc7616a7e01aaecc51ae0ac99fd
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-26 14:10:24 +0530

    desktop completed

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,9 @@ $('.treatmentSlider').slick({
     slidesToScroll: 1,
     infinite: true,
   });
+$('.reviewSlider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+  });
