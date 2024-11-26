96880728c877afc8ebe57449c040e86499008e33
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-26 15:39:32 +0530

    all completed except navbar

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,48 @@ $('.treatmentSlider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      }
+  ]
   });
 $('.reviewSlider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      }
+  ]
   });
