e17d71f80bd87e9d502c0787bc9855a010a97186
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 16:58:00 +0530

    butt fat injections page added

---
+++ buttFatInjection.php
@@ -0,0 +1,148 @@
+<?php
+$pageTitle = "Get Curves with Butt Fat Injections | Safe Buttock Injection";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Butt Fat Injections</h1>
+            <span class="text-warning">Butt Fat Injections</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="">
+                    <h2 class="h1 fw-bold text-uppercase">Butt Fat Injections</h2>
+                    <p class="lh">Butt fat injections, also known as the Brazilian Butt Lift (BBL), are a popular cosmetic procedure designed to enhance and shape your buttocks using your own body fat. This natural augmentation method offers a safe and effective way to achieve fuller, more contoured curves without the need for implants.</p>
+                    <p class="lh">Buttock injections involve transferring fat from one area of your body, such as the abdomen, thighs, or flanks, to your buttocks. This dual-benefit procedure not only enhances your buttocks but also slims down the donor areas, giving you a more balanced and sculpted figure.</p>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/gMjUg-f61A0"></iframe>
+                    <p class=" mt-5"><a href="" class="btn-default">SEE RESULTS</a></p>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- slot-Banner -->
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- How Does Buccal Fat Removal Differ from Cheek Liposuction? -->
+<section class="x-section">
+    <div class="container">
+        <h2 class="h1 fw-bold text-uppercase">How does the Buttock injection Work?</h2>
+        <p class="lh">The butt fat injection procedure involves a precise combination of liposuction, fat purification, and careful fat injection to sculpt and enhance your buttocks naturally, ensuring a harmonious and balanced figure. Here’s how it works:-</p>
+        <ul class="lh list-unstyled mb-4">
+            <li class="mb-2"><strong>Consultation and Planning: </strong>TDuring your initial consultation, a qualified surgeon will assess your goals, discuss your desired outcome, and determine the best areas for fat harvesting.</li>
+            <li class="mb-2"><strong>Liposuction: </strong>The procedure begins with liposuction, where excess fat is gently removed from the predetermined donor areas using a cannula. This process is performed under anesthesia to ensure your comfort.</li>
+            <li class="mb-2"><strong>Purification: </strong>The extracted fat is then purified to remove any impurities, ensuring that only the healthiest fat cells are used for the injections.</li>
+            <li class="mb-2"><strong>Fat Injection: </strong>The purified fat is carefully injected into specific areas of your buttocks to achieve the desired shape and volume. The surgeon will meticulously sculpt the area to create a natural, proportional look.</li>
+            <li class="mb-2"><strong>Recovery: </strong>Recovery time varies, but most patients can return to normal activities within a few weeks. Your surgeon will provide detailed post-operative instructions to ensure optimal healing and results.</li>
+           
+        </ul>
+    </div>
+ </section>
+
+
+
+
+<!-- Who Is a Good Candidate? -->
+
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-center">
+                <div class="">
+                    <h2 class="h1 fw-bold text-uppercase">Benefits of Butt Fat Injections</h2>
+                    <ul class="lh ps-3">
+                        <li><strong>Natural Results: </strong>Because the procedure uses your own fat, the results look and feel more natural compared to synthetic implants.</li>
+                        <li><strong>Dual Contouring: </strong>Buttock injection not only enhance your buttocks but also contour the areas where fat is removed, giving you a more harmonious figure.</li>
+                        <li><strong>Minimal Scarring: </strong>The procedure involves small incisions for liposuction and fat injection, resulting in minimal scarring.</li>
+                        <li><strong>Lower Risk of Complications: </strong>Since the fat comes from your own body, there’s a reduced risk of allergic reactions or rejection.</li>
+                    </ul>
+                    
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/gMjUg-f61A0"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- adv -->
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase mb-3">Are You a Candidate?</h2>
+                    <p class="lh mb-3">Ideal candidates for butt fat injections are individuals who:</p>
+                        <ul class="lh ps-4">
+                            <li class="mb-2">Have enough excess fat in other areas of the body for harvesting.</li>
+                            <li class="mb-2">Are in good overall health and have realistic expectations for the outcome.</li>
+                            <li class="mb-2">Desire a more natural enhancement without synthetic implants.</li>
+                            <li class="mb-2">Want to improve the contour of both their buttocks and donor areas.</li>
+                        </ul>
+
+                   <h2 class="text-uppercase fw-bold text-center">What to Expect After the Procedure</h2>
+                    <p class="lh">After the procedure, it’s normal to experience some swelling, bruising, and discomfort in both the donor and injection sites. Your surgeon will recommend wearing a compression garment to help reduce swelling and support your new shape. Over the next few months, the fat cells will settle, and you’ll see the full results of your buttock injections.</p>
+                    </div>                 
+                    
+                </div>
+
+    </div>  
+</section>
+
+ <!-- why choose us -->
+
+ <section class="x-section pt-2 pb-4">
+    <div class="container">
+        <h2 class="h1 fw-bold text-uppercase text-center">Why Choose Us?</h2>
+        <p class="lh">Our team of experienced, board-certified surgeons specializes in body contouring procedures, including butt fat injections. We prioritize your safety, comfort, and satisfaction, ensuring that you achieve the results you desire. We use advanced techniques and personalized care to provide you with a natural, enhanced look that complements your body.</p>
+
+        <p class="lh">
+        If you’re ready to enhance your curves naturally with butt fat injections, contact us today to schedule a consultation. Let’s work together to achieve the shape you’ve always wanted!
+        </p>
+
+    </div>
+ </section>
+
+
+<?php
+include('footer.php');
+?>
