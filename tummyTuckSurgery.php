5b92a52c8c857d24b2a7da122f7b0c0631c59d13
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 14:19:35 +0530

    tummy tuck surgery page added

---
+++ tummyTuckSurgery.php
@@ -0,0 +1,219 @@
+<?php
+$pageTitle = "Cut Your Waistline with Tummy Tuck Surgery in Gurgaon";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Tummy Tuck Surgery</h1>
+            <span class="text-warning">Tummy Tuck Surgery</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="">
+                    <h2 class=" fw-bold text-uppercase">Tummy Tuck Surgery : All You Need to Know</h2>
+                    <p class="lh"><strong>Tummy tuck surgery</strong>, or <strong>abdominoplasty</strong>, is a cosmetic procedure aimed at improving the appearance of the abdomen. This surgery is popular among individuals who have excess skin and fat in the abdominal area or weakened muscles due to aging, pregnancy, significant weight loss, or genetic factors.</p>
+
+                    <p class="lh">The primary goal of loose skin surgery is to remove excess skin and fat from the middle and lower abdomen. The procedure also tightens the muscles of the abdominal wall, resulting in a flatter and firmer abdomen. It enhances the overall appearance of the abdominal area, providing a more toned and youthful look. </p>
+                   
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/p8uehsq2yYc"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">SEE RESULTS</a></p>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- slot-Banner -->
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Types of Tummy Tuck Procedures -->
+
+<section class="x-section">
+    <div class="container">
+        <h2 class=" text-uppercase fw-bold">Types of Tummy Tuck Procedures</h2>
+        <ul class="lh ps-4">
+            <li class="mb-3"><strong>Full Tummy Tuck: </strong>LInvolves a horizontal incision between the hipbones and another around the navel to remove skin and fat and tighten muscles.</li>
+            <li class="mb-3"><strong>Mini Tummy Tuck: </strong>Targets the area below the navel with a smaller incision, ideal for those with less excess skin and fat.</li>
+            <li class="mb-3"><strong>Extended Tummy Tuck: </strong>Includes a full tummy tuck and extends to the flanks or love handles, beneficial for those with significant excess skin.</li>
+            <li class="mb-3"><strong>Circumferential Tummy Tuck: </strong>Encompasses the entire waistline, typically performed on patients who have undergone massive weight loss.</li>
+        </ul>
+
+    </div>
+</section>
+
+<!-- How Does the Tummy Tuck Surgery Work? -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h2 class=" fw-bold text-uppercase">How Does the Tummy Tuck Surgery Work?</h2>
+                    
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Anesthesia:</strong> The loose skin surgery begins with the administration of anesthesia. General anesthesia is typically used to ensure the patient is comfortable and pain-free during the procedure.</li>
+                        <li class="pb-2"><strong>Incisions:</strong> The surgeon makes incisions to remove excess skin and fat and to tighten abdominal muscles. The length and shape of the incision depend on the amount of excess skin to be removed.</li>
+                        <li class="pb-2"><strong>Reshaping and Suturing:</strong> The abdominal muscles are pulled together and stitched in place. The skin is then stretched down, and excess skin is trimmed.</li>
+                        <li class="pb-2"><strong>Closing Incisions:</strong> The incisions are closed with sutures, skin adhesives, or clips. The surgeon may place temporary drainage tubes to remove excess fluid and reduce swelling.</li>
+                       
+                        
+                       
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/fV7tkmtKyTQ"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- adv -->
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase mb-2">Recovery Post Tummy Tuck Surgery</h2>
+                    <p class="lh">After the procedure, the patient is taken to a recovery area for monitoring. Pain, swelling, and discomfort are managed with medication.</p>
+                   <ul class="lh">
+                    <li class="mb-2"><strong>Initial Healing: </strong>Takes a few weeks, with swelling and discomfort managed by medication.</li>
+                    <li class="mb-2"><strong>Physical Activity: </strong>Patients are advised to avoid strenuous activity and heavy lifting for several weeks.</li>
+                    <li class="mb-2"><strong>Follow-Up: </strong>Regular follow-up visits to monitor healing and address any concerns.</li>
+                    
+                   </ul>
+                  
+                 
+                    
+                </div>
+
+    </div>  
+</section>
+
+<!-- Images -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"><img src="./assets/images/tummyTuck.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+</section>
+
+
+<!-- pre-surgery instructions -->
+
+<section class="x-section">
+    <div class="container">
+    <h2 class="h1 fw-bold text-uppercase">How to Prepare for Tummy Tuck Surgery</h2>
+                    
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Choose a Board-Certified Plastic Surgeon:</strong>  Ensure your surgeon is experienced and certified by a reputable medical board.</li>
+                        <li class="pb-2"><strong>Discuss Your Goals:</strong> Clearly communicate your expectations and desired outcomes.</li>
+                        <li class="pb-2"><strong>Medical Evaluation:</strong> The surgeon will review your medical history, conduct a physical exam, and possibly order lab tests to assess your suitability for the procedure.</li>
+                        <li class="pb-2"><strong>Stay Informed:</strong> Ensure you understand the procedure, potential risks, and the recovery process. Ask your surgeon any last-minute questions.</li>
+                        <li class="pb-2"><strong>Relax:</strong> Stay calm and positive. Practice relaxation techniques such as deep breathing or meditation.</li>                   
+                       
+                    </ul>
+    </div>
+</section>
+
+
+<!-- Breast Reduction: The Procedure -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <iframe class="default-frame" src="https://youtube.com/embed/LUMfatcR4jw"></iframe>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/x4IaotMfFC0"></iframe>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Ideal Candidiate -->
+<section class="x-section pt-0 pb-3">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">Ideal Candidates for Tummy Tuck Surgery</h2>
+                    <ul class="lh">
+                        <li class="mb-2"><strong>Non-Smokers:</strong> Smoking can impede healing and increase the risk of complications. Ideal candidates should be non-smokers or willing to quit well before the surgery.</li>
+                        <li class="mb-2"><strong>Stable Weight:</strong> Candidates should be at or near their ideal weight and have maintained a stable weight for several months. Significant weight fluctuations can affect the surgery’s results.</li>
+                        <li class="mb-2"><strong>Post-Pregnancy: </strong>Women who have had multiple pregnancies may benefit from a tummy tuck to tighten abdominal muscles and remove excess skin.</li>
+                        <li class="mb-2"><strong>After Significant Weight Loss: </strong>Individuals who have lost a substantial amount of weight may have excess, sagging skin that a tummy tuck can remove.</li>
+                        <li class="mb-2"><strong>Diastasis Recti: </strong>Candidates with separated abdominal muscles, often due to pregnancy or significant weight fluctuations, can benefit from the muscle repair aspect of a tummy tuck.</li>
+                        <li class="mb-2"><strong>Skin Elasticity: </strong>Candidates with loose or sagging abdominal skin due to aging, pregnancy, or weight loss are ideal.</li>
+                        <li class="mb-2"><strong>No Serious Medical Conditions: </strong>Candidates should not have any serious health conditions that could impair healing or increase surgical risks.</li>
+                    </ul>
+                </div>
+    </div>
+</section>
+
+
+<!-- Risks and Considerations -->
+<section class="x-section pb-3">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="text-center  fw-bold text-uppercase">Risks and Considerations</h2>
+                    <ul class="lh">
+                        <li class="mb-2"><strong>Scarring:</strong> While the scars can fade over time, they are permanent.</li>
+                        <li class="mb-2"><strong>Complications:</strong> As with any surgery, there are risks such as infection, bleeding, and complications from anesthesia.</li>
+                        <li class="mb-2"><strong>Realistic Expectations: </strong>It’s essential for patients to have realistic expectations and understand that while the procedure can dramatically improve the appearance, it may not achieve perfection.</li>
+                    </ul>
+
+                    <p class="lh">Consulting with a board-certified plastic surgeon can assist people understand the procedure, benefits, and risks, as well as ensure that surgery is performed correctly for their specific needs.</p>
+                </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php
+include('footer.php');
+?>
