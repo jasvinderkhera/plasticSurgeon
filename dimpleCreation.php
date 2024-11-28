986fbcede950881dcc25bb0521c95ed666d38718
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 12:42:13 +0530

    dimple creation page added

---
+++ dimpleCreation.php
@@ -0,0 +1,154 @@
+<?php
+$pageTitle = "Dimple Creation Surgery – Cute Smile with Dimples On Cheeks";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Dimple Creation</h1>
+            <span class="text-warning">Dimple Creation</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">Dimple Creation </h2>
+                    <p class="lh">Dimples have long been associated with charm and beauty, adding a unique touch to your smile. Whether you’ve always dreamed of having dimples or want to enhance your existing ones, our dimple creation surgery can make that dream a reality.</p>
+                    <p class="lh">Dimple creation surgery, also known as dimpleplasty, is a simple, minimally invasive procedure that creates natural-looking dimples on your cheeks. This cosmetic surgery is tailored to your facial structure, ensuring that the dimples appear in just the right place to enhance your smile. Whether you’re looking for a single dimple or a pair, this procedure can give you the desired result with minimal downtime.</p>
+                   
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/gMjUg-f61A0"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">SEE RESULTS</a></p>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h3 class=" fw-bold text-uppercase">Why Choose Dimple Creation?</h3>
+                    <p class="lh">Dimples are often seen as a symbol of beauty and youthful energy. Here’s why many opt for this procedure:</p>
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Enhanced Facial Aesthetics:</strong> Dimples on cheek can make your face more expressive and attractive.</li>
+                        <li class="pb-2"><strong>Quick and Safe Procedure:</strong> Dimple creation is a relatively quick procedure with a short recovery period.</li>
+                        <li class="pb-2"><strong>Customizable Results:</strong> The size and placement of the dimples can be customized to suit your facial features.</li>
+                        <li class="pb-2"><strong>Permanent Yet Subtle:</strong>  The results are permanent, giving you a natural look that doesn’t feel overdone.</li>
+                        
+                       
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/gMjUg-f61A0"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">Can Dimples Add Extra Beauty to Your Face?</h2>
+                    <p class="lh">Dimples are often seen as a charming feature that can enhance the natural beauty of your face. These small indentations, typically visible when you smile, have a unique way of adding character and warmth to your expression. Here’s how dimples can contribute to your overall facial aesthetics:</p>
+                   
+                    <ul class="ps-4 lh">
+                    
+                        <li class="mb-3"><strong>Accentuating Your Smile:</strong> Dimples naturally draw attention to your smile, making it appear more engaging and lively. They can add an extra layer of depth to your expressions, making you appear more approachable and friendly.</li>
+                        <li class="mb-3"><strong>Enhancing Facial Symmetry:</strong> When placed strategically, dimples can create a pleasing balance on your face, enhancing its symmetry. This balance often aligns with what is perceived as aesthetically pleasing, adding to your overall attractiveness.</li>
+                        <li class="mb-3"><strong>Youthful Appearance: </strong> Dimples are often associated with youth and vitality. By adding dimples on cheek, your face can take on a more youthful and vibrant look, helping you maintain a fresh appearance.</li>
+
+                        <li class="mb-3"><strong>Subtle Uniqueness:</strong> Dimples are relatively rare, which can make your face stand out in a crowd. This subtle yet distinctive feature can give you a unique charm that sets you apart.</li>
+                        <li class="mb-3"><strong>Boosting Self-Confidence:</strong> Many people feel more confident when they love the way they look. Dimples can enhance your facial features in a way that boosts your self-esteem, making you feel more comfortable and confident in your skin.</li>
+                    </ul>
+                    <p class="lh">Whether you’ve always admired dimples on others or want to enhance your own smile, dimples can certainly add an extra touch of beauty to your face. With dimple creation surgery, you can achieve this attractive feature and enjoy the benefits of a more expressive, youthful, and unique look.</p>
+                  
+                   
+                    
+                </div>
+    </div>
+</section>
+
+
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase">What to Expect During the Procedure?</h2>
+                   <p class="lh text-center">Our skilled surgeons will begin by marking the precise location of the dimples on your cheeks. The procedure itself is straightforward and usually takes about 30 minutes to an hour under local anesthesia. A small incision is made inside the cheek, and a suture is used to create a depression in the desired location. The incision is hidden inside the mouth, leaving no visible scars.</p>
+                  
+                   <h2 class=" fw-bold text-center text-uppercase">Recovery and Aftercare</h2>
+                   <p class="lh text-center">Recovery from dimple creation surgery is typically quick, with most patients returning to their normal activities within a day or two. You may experience slight swelling or bruising, but this usually subsides within a week. Our team will provide you with detailed aftercare instructions to ensure a smooth recovery and optimal results.</p>
+                    
+                </div>
+    </div>  
+</section>
+
+<section class="x-section">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">Why Choose “Konarc Aesthetics” for Your Dimple Creation Surgery?</h2>
+                    <p class="lh">Why trust your smile to anyone else? Discover why ‘Konarc Aesthetics’ is the top choice for expert dimple creation surgery.</p>
+                   
+                    <ul class="ps-4 lh mb-4">
+                    
+                        <li class="mb-3"><strong>Experienced Surgeons:</strong> Our team of board-certified plastic surgeons specializes in facial cosmetic procedures, ensuring that you’re in expert hands.</li>
+                        <li class="mb-3"><strong>Personalized Care:</strong> We believe in providing personalized care, tailoring each procedure to meet your specific aesthetic goals.</li>
+                        <li class="mb-3"><strong>State-of-the-Art Facility: </strong>  Our clinic is equipped with the latest technology and adheres to the highest safety standards, ensuring a comfortable and secure environment for your surgery.</li>
+
+                        <li class="mb-3"><strong>Comprehensive Consultation:</strong> We offer detailed consultations to discuss your goals, answer your questions, and guide you through the entire process.</li>
+                      
+                    </ul>
+
+                    <h4 class="fw-bold text-uppercase text-center">Ready to Enhance Your Smile?</h4>
+                    <p class="lh">If you’ve been considering dimple creation surgery, we’re here to help you achieve the look you desire. Schedule a consultation with our expert team today to learn more about the procedure and take the first step towards a more confident smile.</p>
+                    <p class="lh">Reach out to us to schedule your consultation or to learn more about dimple creation surgery. Our team is here to answer all your questions and help you on your journey to a brighter, more charming smile.</p>
+                  
+                   
+                    
+                </div>
+    </div>
+</section>
+
+
+
+
+<?php
+include('footer.php');
+?>
