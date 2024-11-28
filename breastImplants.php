4d9c4eb6ffbf183d46c0157d6d60c3418fdcc351
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 13:10:34 +0530

    breast Implants page added

---
+++ breastImplants.php
@@ -0,0 +1,173 @@
+<?php
+$pageTitle = "Breast Implants in Gurgaon | Natural Big Boobs";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Breast Implants</h1>
+            <span class="text-warning">Breast Implants</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="">
+                    <h2 class=" fw-bold text-uppercase">Breast Implants: Enhancing Your Confidence</h2>
+                    <p class="lh">Breast implants are a popular cosmetic procedure designed to enhance the size, shape, and overall appearance of the breasts. Whether you’re looking to increase volume, restore fullness lost after pregnancy or weight loss, or achieve better symmetry, breast implants can help you achieve your desired look.</p>
+
+                    <p class="lh">Breast implants are placed under the breast tissue or chest muscles to augment the size or shape of the breasts. They come in various types, shapes, and sizes, allowing for a highly customizable approach to breast enhancement.</p>
+                   
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/3hU7wn8UwKs"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">SEE RESULTS</a></p>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Procedure -->
+<section class="x-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h2 class=" fw-bold text-uppercase">The procedure</h2>
+                    <p class="lh">Breast implant surgery in Gurgaon is typically performed under general anesthesia and takes about 1 to 2 hours. During the procedure, the surgeon makes an incision, creates a pocket either under the breast tissue or the chest muscle, and then places the implant inside. The incision is then closed with sutures. The location of the incision can vary depending on the type of implant, your anatomy, and your surgeon’s recommendation. Common incision sites include:</p>
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Inframammary:</strong> Under the breast, along the natural crease.</li>
+                        <li class="pb-2"><strong>Periareolar:</strong> Around the edge of the areola.</li>
+                        <li class="pb-2"><strong>Transaxillary:</strong>  In the armpit.</li>
+                        <li class="pb-2"><strong>Transumbilical (TUBA):</strong>  Through the navel (used less commonly).</li>
+                        
+                       
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/eSE55QDKxqQ"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- adv -->
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase">Recovery and Results</h2>
+                   <p class="lh text-center">Recovery from breast implant surgery typically involves some discomfort, swelling, and bruising for the first few days. Most patients can return to work and normal activities within a week, though strenuous activities should be avoided for at least 4 to 6 weeks.</p>
+                  
+                   <p class="lh text-center">The final results of your breast implant surgery will become more apparent as the swelling subsides. The implants will settle into place over the following months, revealing the final shape and size. Scars from the incisions will gradually fade over time.</p>
+                    
+                </div>
+    </div>  
+</section>
+
+
+<!-- Images -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+        </div>
+    </div>
+</section>
+
+<!-- Risks -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h2 class=" fw-bold text-uppercase">Risks and Considerations</h2>
+                    <p class="lh">While breast implants are generally safe, it’s important to be aware of potential risks and complications, including:</p>
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Capsular Contracture:</strong> The formation of scar tissue around the implant, which can cause the breast to feel firm or appear misshapen.</li>
+                        <li class="pb-2"><strong>Implant Rupture or Leakage:</strong> The formation of scar tissue around the implant, which can cause the breast to feel firm or appear misshapen.</li>
+                        <li class="pb-2"><strong>Changes in Sensation:</strong> Some women experience changes in nipple or breast sensation, which can be temporary or permanent.</li>
+                        <li class="pb-2"><strong>Infection:</strong>  As with any surgery, there is a risk of infection, which may require additional treatment or surgery.
+                        </li>
+                        
+                       
+                    </ul>
+                    <p class="lh">Regular follow-up visits with your surgeon are essential to monitor the condition of your implants and ensure they continue to meet your expectations.</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/3i7n02gNcT0"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- choose right implant -->
+<section class="x-section py-0">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="text-center fw-bold text-uppercase">Choosing the Right Implant for You</h2>
+                    <p class="lh">Selecting the right breast implant involves considering factors such as your body type, lifestyle, and aesthetic goals. During your consultation, your surgeon will discuss the various options and recommend the most suitable implant type, size, and placement for your unique needs.</p>
+                   
+                    
+                    <p class="lh">At <strong>Konarc Aesthetics</strong>, we are committed to helping you achieve the look you desire with the highest standards of care and expertise. Our experienced surgeons work closely with you to understand your goals and tailor the procedure to ensure natural-looking, beautiful results. From the initial consultation to post-operative care, we are with you every step of the way.</p>
+
+                    <p class="lh"><strong>Contact us today</strong> to start your transformation for breast implant surgery in Gurgaon and embrace the confidence that comes with feeling great about your body.</p>                    
+                </div>
+    </div>
+</section>
+
+<!-- reviews -->
+<section class="x-section pt-0">
+    <div class="container">
+    <?php 
+        include('reviews.php');
+        ?>
+    </div>
+</section>
+
+
+
+
+
+<?php
+include('footer.php');
+?>
