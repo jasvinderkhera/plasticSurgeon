8137a2b0df4a6e6e8fb184708fa13343155a2236
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 13:51:57 +0530

    breast reduction page added

---
+++ breastReduction.php
@@ -0,0 +1,239 @@
+<?php
+$pageTitle = "Breast Reduction Surgery in Gurgaon | Beautiful Boobs";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Breast Reduction</h1>
+            <span class="text-warning">Breast Reduction</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="">
+                    <h2 class=" fw-bold text-uppercase">Understanding Breast Reduction</h2>
+                    <p class="lh">Breast reduction, also known as reduction mammaplasty, is a surgical procedure designed to reduce the size and weight of large breasts. This surgery is often sought by individuals experiencing physical discomfort, emotional distress, or self-consciousness due to their breast size.</p>
+
+                    <p class="lh">Breast reduction can be life-changing, providing significant physical and emotional benefits. It’s essential to consult with a qualified plastic surgeon to determine if this procedure aligns with your health needs and personal goals.</p>
+                   
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/3hU7wn8UwKs"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">SEE RESULTS</a></p>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- slot-Banner -->
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Why Consider Breast Reduction? -->
+
+<section class="x-section">
+    <div class="container">
+        <h2 class=" text-uppercase fw-bold">Why Consider Breast Reduction?</h2>
+        <ul class="lh ps-4">
+            <li class="mb-3"><strong>Physical Relief: </strong>Large breasts can cause a range of physical problems, including back, neck, and shoulder pain. </li>
+            <li class="mb-3"><strong>Improved Mobility: </strong>For many, large breasts can hinder physical activity and exercise.</li>
+            <li class="mb-3"><strong>Enhanced Appearance: </strong>Some individuals feel self-conscious about their breast size. Breast reduction can help achieve a more proportionate figure.</li>
+            <li class="mb-3"><strong>Better Clothing Fit: </strong>Finding clothes that fit well can be challenging for those with large breasts.</li>
+        </ul>
+
+        <p class="lh">If these points resonate with you, considering breast reduction could be beneficial.</p>
+    </div>
+</section>
+
+<!-- Why Consider Breast Reduction 2 ? -->
+<section class="x-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                <h2 class="h1 text-uppercase fw-bold">Why Consider Breast Reduction?</h2>
+        <ul class="lh ps-4">
+            <li class="mb-3"><strong>Physical Relief: </strong>Large breasts can cause a range of physical problems, including back, neck, and shoulder pain. </li>
+            <li class="mb-3"><strong>Improved Mobility: </strong>For many, large breasts can hinder physical activity and exercise.</li>
+            <li class="mb-3"><strong>Enhanced Appearance: </strong>Some individuals feel self-conscious about their breast size. Breast reduction can help achieve a more proportionate figure.</li>
+            <li class="mb-3"><strong>Better Clothing Fit: </strong>Finding clothes that fit well can be challenging for those with large breasts.</li>
+        </ul>
+
+        <p class="lh">If these points resonate with you, considering breast reduction could be beneficial.</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/eSE55QDKxqQ"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">SEE RESULTS</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- why breast augmentation -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h2 class=" fw-bold text-uppercase">Who Should Consider Breast Reduction?</h2>
+                    
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Poor Posture:</strong> Large breasts can cause poor posture and contribute to spinal issues. Breast reduction can help improve posture and spinal health.</li>
+                        <li class="pb-2"><strong>Shoulder Grooving:</strong> Deep shoulder grooves from bra straps due to heavy breasts can be relieved with breast reduction surgery.</li>
+                        <li class="pb-2"><strong>Clothing Fit Issues:</strong> Trouble finding well-fitting clothes or bras due to disproportionate breast size can be resolved through breast reduction.</li>
+                        <li class="pb-2"><strong>Self-Esteem Issues:</strong> Individuals who feel self-conscious or suffer from low self-esteem due to their breast size may find breast reduction beneficial.</li>
+                       
+                        
+                       
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/eSE55QDKxqQ"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- pre-surgery instructions -->
+
+<section class="x-section">
+    <div class="container">
+    <h2 class=" fw-bold text-uppercase">Pre-Surgery Instructions</h2>
+                    
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Medical Tests:</strong>  You may need to undergo blood tests, a mammogram, or other diagnostic tests to ensure you’re in good health for surgery.</li>
+                        <li class="pb-2"><strong>Medical Evaluation:</strong> Your surgeon will review your medical history, discuss your goals, and conduct a physical examination.</li>
+                        <li class="pb-2"><strong>Discuss Expectations:</strong> Clearly communicate your expectations and desired outcomes. Your surgeon will explain what can realistically be achieved.</li>
+                        <li class="pb-2"><strong>Understand the Procedure:</strong> Learn about the surgical process, potential risks, and expected recovery time.</li>
+                        <li class="pb-2"><strong>Medications:</strong> Your surgeon will provide a list of medications and supplements to avoid before surgery, as some can increase bleeding risks.</li>
+                        <li class="pb-2"><strong>Smoking:</strong> If you smoke, you’ll need to stop several weeks before the surgery, as smoking can interfere with healing.</li>
+                       
+                        
+                       
+                    </ul>
+    </div>
+</section>
+
+<!-- adv -->
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase mb-2">Recovery Post-Breast Reduction</h2>
+                   <ul class="lh">
+                    <li class="mb-2"><strong>Rest is Crucial: </strong>Take it easy for the first few days post-surgery, avoiding strenuous activities and heavy lifting to allow your body to start the healing process.</li>
+                    <li class="mb-2"><strong>Pain Management: </strong>Use prescribed pain medications as directed to manage discomfort and make the initial recovery period more comfortable.</li>
+                    <li class="mb-2"><strong>Surgical Bra or Compression Garment: </strong>Wear the support garment provided to minimize swelling and support the healing tissues.</li>
+                    <li class="mb-2"><strong>Regular Check-Ups: </strong>Attend all scheduled follow-up appointments to ensure proper healing and address any concerns or complications early.</li>
+                    <li class="mb-2"><strong>Light Activities: </strong> You can begin light activities within a week or two, but avoid anything strenuous or that could strain your chest.</li>
+                   </ul>
+                  
+                 
+                    
+                </div>
+
+    </div>  
+</section>
+
+
+<!-- Images -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+        </div>
+    </div>
+</section>
+
+<!-- Breast Reduction: The Procedure -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h3 class="h1 fw-bold text-uppercase">Breast Reduction: The Procedure</h3>
+                    <p class="lh">Breast reduction surgery typically involves the following steps:</p>
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Consultation:</strong> A thorough discussion with a plastic surgeon to understand your goals, medical history, and the specifics of the procedure.</li>
+
+                        <li class="pb-2"><strong>Surgery:</strong> During the procedure, excess breast tissue, fat, and skin are removed to achieve the desired breast size. The nipples and areolas are often repositioned for a natural look.</li>
+
+                        <li class="pb-2"><strong>Recovery:</strong>  Post-surgery, patients will experience swelling and discomfort, which can be managed with pain medication. Most people can return to work and normal activities within a few weeks, but it may take several months for the breasts to settle into their final shape.</li>
+                       
+                        
+                       
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/3i7n02gNcT0"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Personalized Consultation -->
+<section class="x-section pt-0 pb-3">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="h1 text-center fw-bold text-uppercase">Risks & Considerations in Breast Reduction</h2>
+                    <p class="lh">As with any surgery, breast reduction carries some risks, including infection, scarring, and changes in nipple sensation. It’s crucial to have realistic expectations and discuss potential risks with your surgeon.</p>                
+                    <p class="lh">For the best breast reduction results, consult our expert plastic surgeon. </p>                
+                    <p class="lh">By choosing our experienced team, you’ll receive personalized care tailored to your specific needs. We are committed to ensuring your comfort and satisfaction throughout the process. From the initial consultation to post-operative care, we are dedicated to supporting you every step of the way. Our goal is to help you feel more confident and comfortable.</p>
+                </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php
+include('footer.php');
+?>
