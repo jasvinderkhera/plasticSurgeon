70c3fd5a06b6643f64acf3e2af55e4cee83165bd
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 13:24:26 +0530

    breast aumentation page added

---
+++ breastAugmentation.php
@@ -0,0 +1,173 @@
+<?php
+$pageTitle = "Breast Augmentation | Perfect Big Boob Surgery In Gurgaon";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Breast Augmentation</h1>
+            <span class="text-warning">Breast Augmentation</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="">
+                    <h2 class=" fw-bold text-uppercase">Breast augmentation</h2>
+                    <p class="lh">Breast augmentation is a popular choice to enhance the size, shape, and symmetry of a woman’s breasts. Whether you want to increase volume, restore balance, or achieve a more youthful contour, It offers a customizable solution tailored to your unique goals.</p>
+
+                    <p class="lh">So, what exactly is it? Well, breast increasing surgery also known as augmentation mammoplasty or big boob surgery, involves the surgical placement of implants to improve breast size and shape. This procedure is ideal for women who feel their breasts are too small, uneven, or have lost volume due to factors such as weight loss, pregnancy, or aging.</p>
+                   
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/3hU7wn8UwKs"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">SEE RESULTS</a></p>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- slot-Banner -->
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- types of implants -->
+
+<section class="x-section">
+    <div class="container">
+        <h2 class="h1 text-uppercase fw-bold">types of implants</h2>
+        <p class="lh">There are two primary types of breast implants used in augmentation:</p>
+        <ul class="lh ps-4">
+            <li><strong>Saline Implants: </strong>Filled with sterile salt water, saline implants are adjustable and provide a firm feel. In the rare case of a leak, the body absorbs the saline solution naturally.</li>
+            <li><strong>Silicone Implants: </strong>These are filled with silicone gel, which feels more like natural breast tissue. Silicone implants are popular for their soft and natural look.</li>
+        </ul>
+
+        <p class="lh">When considering breast implants, deciding between saline and silicone comes down to your goals, body type, and preferences for look, feel, and maintenance.</p>
+        <p class="lh">For example, saline implants (filled with a sterile saltwater solution) tend to be firmer than silicone. They can look rounder and may feel less natural, particularly in patients with smaller amounts of natural breast tissue. On the other hand, silicone implants are known for their more natural feel and appearance, and they closely mimic the texture of natural breast tissue. This makes them popular among those looking for a softer, fuller look.</p>
+        <p class="lh">Consulting with a certified plastic surgeon for breast augmentation in Gurgaon can help clarify which implant type best aligns with your aesthetic goals, lifestyle, and body type.</p>
+    </div>
+</section>
+
+<!-- Procedure -->
+<section class="x-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h2 class=" fw-bold text-uppercase">The procedure</h2>
+                    <p class="lh">The breast augmentation in Gurgaon typically takes between one to two hours and is performed under general anesthesia. Your surgeon will make an incision in one of three possible locations: under the breast fold, around the areola, or in the armpit. The chosen incision site depends on your anatomy and the type of implant used. Once the implants are placed, the incisions are closed with sutures, and the recovery process begins.</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/eSE55QDKxqQ"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- adv -->
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase">Recovery and Results</h2>
+                   <p class="lh text-center">Recovery from this surgery usually involves a few days of rest, with most patients resuming normal activities within a week. You may experience some swelling, bruising, and discomfort, but these symptoms typically subside within a few weeks. The final results of your breast augmentation will become more apparent as your body heals and the implants settle into their new position.</p>
+                  
+                 
+                    
+                </div>
+                <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+
+    </div>  
+</section>
+
+
+<!-- Images -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+            <div class="col-md-4"><img src="./assets/images/beforeAfter/img1.png" class="img-fluid" alt=""></div>
+        </div>
+    </div>
+</section>
+
+<!-- why breast augmentation -->
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h3 class=" fw-bold text-uppercase">Why Choose Breast Augmentation?</h3>
+                    
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Enhance Confidence:</strong> Many women choose breast increasing surgery to feel more confident in their appearance, whether in clothing or swimwear.</li>
+                        <li class="pb-2"><strong>Restore Volume:</strong> This procedure can restore lost volume after pregnancy, breastfeeding, or weight loss, giving you a fuller, more youthful silhouette.</li>
+                        <li class="pb-2"><strong>Correct Asymmetry:</strong> If your breasts are naturally uneven in size or shape, boob surgery can help create a more balanced and symmetrical look.</li>
+                       
+                        
+                       
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/3i7n02gNcT0"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Personalized Consultation -->
+<section class="x-section pt-0 pb-3">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="text-center fw-bold text-uppercase">Personalized Consultation</h2>
+                    <p class="lh">Every woman’s body is unique, and boob surgery is a highly individualized procedure. A consultation with a qualified plastic surgeon for breast augmentation in Gurgaon is essential to discuss your goals, choose the right implant type and size, and understand the procedure in detail.</p>                
+                    <p class="lh">Whether you want to enhance your natural beauty or restore your confidence, Breast surgery can help you achieve the desired look. Contact us today to schedule your personalized consultation and take the first step toward the new you.</p>                
+                </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php
+include('footer.php');
+?>
