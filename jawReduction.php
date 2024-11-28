65f95886c9b91dcbe8bc8db836c1a3d40b3762b4
jasvinderkhera <ankitkhera15@gmail.com>
2024-11-28 12:25:19 +0530

    jaw reduction page added

---
+++ jawReduction.php
@@ -0,0 +1,196 @@
+<?php
+$pageTitle = "Jaw Reduction | Jaw Surgery in Gurgaon - Enhance Your Appearance";
+$pageDescription = "";
+$pageKeyword = "";
+include('header.php');
+?>
+
+<section class="section-heading">
+    <div class="overlay">
+        <div class="container-xxl">
+            <h1>Jaw Reduction</h1>
+            <span class="text-warning">Jaw Reduction</span>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">Jaw Reduction </h2>
+                    <p class="lh">The jawline plays a crucial role in the overall balance and harmony of facial features. Both men and women can enhance their facial aesthetics through mandibular surgery, achieving a more refined and appealing jawline shape.</p>
+                    <p class="lh">Jaw reduction, also known as mandibular reduction or V-line surgery, is a cosmetic procedure designed to reshape and contour the jawline for a more balanced and harmonious appearance. This procedure is ideal for individuals with a wide or angular jaw who desire a softer, more feminine look or a refined facial profile.</p>
+                    <p class=" mt-5"><a href="" class="btn-default">SEE RESULTS</a></p>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/gMjUg-f61A0"></iframe>
+                   
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-primary x-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <div class="slot-banner">
+                    <img src="assets/images/msg.svg" class="img-fluid img" alt="">
+                    <div class="text-white">
+                        <span>Get Support</span>
+                        <span class="d-block fw-bold">drpreetiyadav.com@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="text-center text-end">
+                    <h3 class="text-uppercase fw-bold">Book Your Slot Today</h3>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="text-end">
+                    <a href="" class="btn-default">CONTACT US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">How Does the Procedure Work?</h2>
+                    <p class="lh">Jaw reduction surgery is a precise and tailored procedure designed to reshape the lower jawbone, resulting in a slimmer and more contoured jawline. The goal is to create a harmonious balance between the jaw and other facial features, enhancing overall facial aesthetics.</p>
+                   
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Step 1: Anesthesia</strong></p>
+                        <li>The procedure begins with administering general anesthesia to ensure you are comfortable and pain-free throughout the surgery. Your surgeon will discuss the anesthesia process with you during the pre-operative consultation.</li>
+                    </ul>
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Step 2: Incision Placement</strong></p>
+                        <li>The surgeon carefully makes incisions, usually inside the mouth along the lower gum line, to access the jawbone. This approach minimizes visible scarring. In some cases, an additional small incision may be made under the chin, depending on the technique used.</li>
+                    </ul>
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Step 3: Bone Shaving and Contouring</strong></p>
+                        <li>The primary method for jaw reduction involves bone shaving and contouring. Using specialized surgical instruments, the surgeon meticulously removes or reshapes excess bone tissue from the lower jaw. This step is crucial in achieving a slimmer and more tapered jawline. The amount of bone removed is carefully calculated based on your facial structure and desired outcome.</li>
+                    </ul>
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Step 4: Muscle and Tissue Adjustment</strong></p>
+                        <li>In some cases, the surgeon may also adjust the surrounding muscles and soft tissues to enhance the overall contour of the jawline. This step ensures a natural-looking result and a smooth transition between the jawline and other facial features.</li>
+                    </ul>
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Step 5: Closure and Recovery</strong></p>
+                        <li>Once the desired shape is achieved, the incisions are carefully closed with sutures. Since most of the incisions are made inside the mouth, visible scarring is minimal. The recovery process involves managing swelling and discomfort, which typically subsides within a few weeks. Your surgeon will provide detailed post-operative care instructions to ensure a smooth and successful recovery.</li>
+                    </ul>
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Non-Surgical Options</strong></p>
+                        <li>For those seeking a less invasive approach, Botox injections may be an option. Botox can temporarily relax the masseter muscles, which are responsible for a square or prominent jawline. This method is ideal for individuals looking for a non-surgical solution with minimal downtime. However, the results are temporary and require maintenance treatments.</li>
+                    </ul>
+                    <ul class="list-unstyled lh">
+                        <p class="mb-1"><strong>Customized Approach</strong></p>
+                        <li>Every Jaw surgery in Gurgaon is personalized to suit your unique facial structure and aesthetic goals. During your consultation, your surgeon will evaluate your facial anatomy, discuss your expectations, and recommend the most appropriate technique to achieve the desired results.</li>
+                    </ul>
+                   
+                    
+                </div>
+    </div>
+</section>
+<section class="x-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div >
+                    <h2 class=" fw-bold text-uppercase">Benefits of Jaw Reduction Surgery</h2>
+                    
+                    <ul class="lh ps-3">
+                        <li class="pb-2"><strong>Enhanced Facial Harmony:</strong> Achieve a balanced facial profile by reducing the prominence of the jawline.</li>
+                        <li class="pb-2"><strong>Softened Jawline:</strong> Ideal for individuals seeking a more feminine or refined appearance.</li>
+                        <li class="pb-2"><strong>Customizable Results:</strong> The procedure is tailored to your specific facial features and desired outcome.</li>
+                        <li class="pb-2"><strong>Minimal Scarring:</strong> Advanced surgical techniques ensure minimal visible scarring and a quicker recovery time.</li>
+                        <li class="pb-2"><strong>Boosted Confidence:</strong> Many patients experience increased self-confidence with their new, more balanced facial appearance.</li>
+                       
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <iframe class="default-frame" src="https://youtube.com/embed/gMjUg-f61A0"></iframe>
+                    <p class=" mt-5 text-center"><a href="" class="btn-default">FOR MORE INFORMATION</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="x-section pt-0">
+    <div class="container">
+                <div class="adv shadow-lg">
+                    <h2 class=" fw-bold text-center text-uppercase">Who is a Candidate for Jaw Reduction?</h2>
+                   <p class="lh">Jaw reduction is suitable for individuals who:</p>
+                   <ul class="lh">
+                    <li>Have a wide, square, or prominent jawline they wish to refine.</li>
+                    <li>Are in good overall health with realistic expectations for the procedure.</li>
+                    <li>Seek a more balanced and proportionate facial appearance.</li>
+                    <li>Desire a permanent solution to achieve their aesthetic goals.</li>
+                   </ul>
+                   <h2 class=" fw-bold text-center text-uppercase">Recovery and Aftercare</h2>
+                   <p class="lh">After jaw surgery in Gurgaon, most patients subsides within a few weeks. A few may experience swelling and discomfort. Our team will provide detailed aftercare instructions to ensure a smooth recovery. Follow-up appointments will be scheduled to monitor your progress and ensure optimal results. These appointments are crucial for ensuring that your recovery is on track and that you are healing as expected.</p>
+                    
+                </div>
+    </div>  
+</section>
+
+<section class="x-section">
+    <div class="container">
+    <div class="pe-4">
+                    <h2 class="h1 fw-bold text-uppercase">Is Jaw Reduction Surgery Painful?</h2>
+                    <p class="lh">One of the most common concerns for patients considering jaw reduction surgery is the potential for pain during and after the procedure. Understanding what to expect can help alleviate anxiety and prepare you for the recovery process.</p>
+                   
+                    <ul class="lh">
+                    <li><p class="mb-1"><strong>During the Procedure</strong></p>
+                        Jaw reduction surgery is performed under general anesthesia, which means you will be completely unconscious and unaware of any sensations during the procedure. The anesthesia ensures that you do not feel any pain while the surgeon is reshaping your jawbone. The use of general anesthesia is a standard practice in this type of surgery to ensure patient comfort and safety.</li>
+                    </ul>
+                    <ul class="lh">
+                    <li> <p class="mb-1"><strong>Post-Operative Discomfort</strong></p>
+                        After the surgery, it is normal to experience some discomfort as your body begins to heal. This discomfort is usually managed with prescribed pain medication, which your surgeon will provide. The level of pain can vary depending on the extent of the surgery and your individual pain tolerance, but it is generally described as mild to moderate.</li>
+                    </ul>
+                    <ul class="lh">
+                    <li> <p class="mb-1"><strong>Swelling and Bruising</strong></p>
+                        Swelling is a common side effect of jaw reduction surgery and can contribute to discomfort. The swelling typically peaks a few days after the surgery and then gradually subsides over the following weeks. Bruising may also occur but is usually minimal and resolves within a week or two. Your surgeon will give you specific instructions on how to manage swelling, such as applying cold compresses and keeping your head elevated.</li>
+                    </ul>
+                    <ul class="lh">
+                    <li><p class="mb-1"><strong>Recovery Period</strong></p>
+                      During the initial recovery period, you may experience tightness or stiffness in your jaw as the tissues heal. This is a normal part of the healing process and should improve as you recover. Most patients can return to their normal activities within two weeks, although strenuous activities should be avoided until your surgeon gives you the all-clear.</li>
+                    </ul>
+                    <ul class="lh">
+                    <li><p class="mb-1"><strong>Long-Term Comfort</strong></p>
+                        While the first few days after surgery may involve some discomfort, it is important to note that pain is usually temporary and well-managed with medication. As your jaw heals and the swelling decreases, most patients report feeling much more comfortable. The long-term results of the surgery—a slimmer, more contoured jawline—are well worth the short-term discomfort for many individuals.</li>
+                    </ul>
+                
+                   
+                    
+                </div>
+    </div>
+</section>
+<section class="x-section pt-0">
+    <div class="container">
+        <div class="row">
+           
+            <div class="col-md-8">
+                <h2 class="text-center text-uppercase h1 fw-bold">Why Choose Us?</h2>
+                <p class="lh">At <strong>Konarc Aesthetics</strong>, we prioritize patient safety, comfort, and satisfaction. Our experienced surgeons utilize the latest techniques and technologies to deliver exceptional results. We are committed to providing personalized care and achieving the natural, harmonious look you desire.</p>
+                <p class="fw-bold text-start">Consult with Our Experts for Jaw Surgery</p>
+                <p class="lh">Ready to take the first step towards a more refined jawline? Schedule a consultation with our experts today. We’ll discuss your goals, evaluate your facial structure, and develop a customized treatment plan to help you achieve the look you’ve always wanted.</p>
+                <p class="lh">Don’t let a prominent jawline hold you back from feeling your best. Reach out to us today to learn more about jaw reduction surgery and start your journey toward a more balanced, confident appearance.</p>
+            </div>
+            <div class="col-md-4"> 
+                <img src="./assets/images/jawReduction.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php
+include('footer.php');
+?>
